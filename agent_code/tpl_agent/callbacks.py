091b2ba4ebd1bd0cad67c7eb4381e4323df889bd
neelkanthrawat <neelkanthrawat3992@gmail.com>
2024-08-15 08:17:54 +0200

    some minor changes in callbacks of tpl_agent (I yet need to adjust the code for both 1d and 2d state representation)

--- agent_code/tpl_agent/callbacks.py
+++ agent_code/tpl_agent/callbacks.py
@@ -7,13 +7,15 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from .states_to_features import state_to_features
+from .states_to_features import state_to_features, state_to_features_encoder
 from .neural_agent import DQNCNN, DQNMLP
+from .rule_based_agent_action import rule_based_action, look_for_targets, reset_self
+from .autoencoder_feature_reduction import Autoencoder
 
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
-
-
+AE_WEIGHTS_PATH = 'D:\\Desktop\\master_scientific_computing\\second_semester\\ml essentials\\Final Project\\ML-essentials-2024-BomberMan\\ae_model_weights_7x7.pth'
+AGENT_SAVED = 'my-saved-model-7x7-with-ae.pt'
 
 def setup(self):
     """
@@ -32,16 +34,25 @@ def setup(self):
     print("First checking Callbacks: setup")
     input_size = 7 * 7 * 6  # Adjust this based on input size (example: 8x8 field with 6 channels)
     num_actions = len(ACTIONS)
-    hidden_layers_sizes = [64,64,64,32]  # Example hidden layer sizes hidden layer (older):[64,16]
-
-    if self.train or not os.path.isfile("my-saved-model.pt"):
+    hidden_layers_sizes = [12,8]  # Example hidden layer sizes hidden layer (older):[64,16]
+    ### add info for the autoencoder; we will use it for feature reduction and space representation
+    hidden_layers_ae_list = [124*2]
+    code_space_dim_ae = 20
+    self.device= 'cpu' ### SETTING UP THE DEVICE ### LATER WE WILL CHANGE IT TO CUDA
+    if self.train or not os.path.isfile(AGENT_SAVED):
+        self.ae = Autoencoder(input_size,hidden_layers_ae_list, code_space_dim_ae)
         self.logger.info("Setting up model from scratch.")
-        self.model = DQNMLP(input_size, num_actions, hidden_layers_sizes)
+        self.model = DQNMLP(code_space_dim_ae, num_actions, hidden_layers_sizes)
+        self.ae.load_state_dict(torch.load(AE_WEIGHTS_PATH, map_location=torch.device(self.device)))
+        self.ae.eval()
     else:
         self.logger.info("Loading model from saved state.")
-        self.model = DQNMLP(input_size, num_actions, hidden_layers_sizes)
-        self.model.load_state_dict(torch.load("my-saved-model.pt"))
+        self.model = DQNMLP(code_space_dim_ae, num_actions, hidden_layers_sizes)
+        self.model.load_state_dict(torch.load(AGENT_SAVED))
         self.model.eval()
+        self.ae = Autoencoder(input_size,hidden_layers_ae_list, code_space_dim_ae)
+        self.ae.load_state_dict(torch.load(AE_WEIGHTS_PATH, map_location=torch.device(self.device)))
+        self.ae.eval()
     
 
 
@@ -64,18 +75,22 @@ def act(self, game_state: dict) -> str:
     if self.train:
         self.epsilon = max(self.epsilon_end, self.epsilon * self.epsilon_decay)
         
-        if random.random() < self.epsilon:
+        if random.random() < self.epsilon: ### 
             self.logger.debug("Choosing action purely at random for exploration.")
             actions_for_coin_collection=['UP', 'RIGHT', 'DOWN', 'LEFT']
             action=np.random.choice(actions_for_coin_collection)
             # print(f"\nACTION: {action}")
             return action
         else:
+            # using rule based agent to assist us in faster training
+            # self.logger.debug("Choosing action based on rule-based logic for exploitation.")
+            # return rule_based_action(self,game_state)
             self.logger.debug("Choosing action based on model prediction for exploitation.")
             #return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .2, 0.0])### PLACEHOLDER
             # TODO: I NEED TO CHECK HOW TO INCORPORATE ACTION IN THE NN MODEL FOR DQN. THEN I WILL FILL THIS PART OF THE CODE
-            state = state_to_features(game_state)
-            state = state.clone().detach().unsqueeze(0).float()#torch.tensor(state, dtype=torch.float32).unsqueeze(0)  # Add batch dimension
+            # state = state_to_features(game_state)
+            # state = state.clone().detach().unsqueeze(0).float()#torch.tensor(state, dtype=torch.float32).unsqueeze(0)  # Add batch dimension
+            state = state_to_features_encoder(self, game_state)
             with torch.no_grad():
                 q_values = self.model(state)
             action_index = torch.argmax(q_values).item()
@@ -87,8 +102,9 @@ def act(self, game_state: dict) -> str:
         self.model.eval()
         self.logger.debug("Choosing action using trained model (non-training mode).")
         # return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .2, 0.0])
-        state = state_to_features(game_state)
-        state = state.clone().detach().unsqueeze(0).float()#torch.tensor(state, dtype=torch.float32).unsqueeze(0)  # Add batch dimension
+        # state = state_to_features(game_state)
+        # state = state.clone().detach().unsqueeze(0).float()#torch.tensor(state, dtype=torch.float32).unsqueeze(0)  # Add batch dimension
+        state = state_to_features_encoder(self, game_state)
         with torch.no_grad():
             q_values = self.model(state)
         action_index = torch.argmax(q_values).item()
